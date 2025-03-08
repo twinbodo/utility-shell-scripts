c38c635df8036b359b1c7f67d9b0c4a94c0eb589
twinbodo <twinbodo@gmail.com>
2025-03-08 23:54:25 +0530

    update bluetooth script

--- bluetooth_manager.sh
+++ bluetooth_manager.sh
@@ -11,15 +11,15 @@ export BLUEUTIL_ALLOW_ROOT=0
 # Get detailed Bluetooth device information
 bluetooth_info=$(/usr/sbin/system_profiler SPBluetoothDataType)
 
-log_message "Bluetooth info: $bluetooth_info"
+#log_message "Bluetooth info: $bluetooth_info"
 
 connected_section=$(echo "$bluetooth_info" | awk '/Connected:/,/Not Connected:/')
-log_message "connected section: $connected_section"
+#log_message "connected section: $connected_section"
 
 # Check if any Bluetooth devices are connected
 connected_devices=$(echo "$connected_section" | grep "Minor Type:")
 
-log_message "connected_devices: $connected_devices"
+#log_message "connected_devices: $connected_devices"
 
 if [[ -n "$connected_devices" ]]; then
     # There are devices connected, ensure Bluetooth stays on
