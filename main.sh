9263e8efcbd160775e75e657219a0fbf342314c1
twinbodo <twinbodo@gmail.com>
2025-03-31 00:50:32 +0530

    add: git reset function

--- main.sh
+++ main.sh
@@ -159,14 +159,29 @@ install_zsh_autosuggestions() {
     fi
 }
 
+move_back_n_commits() {
+    read -p "Enter the number of commits to move back: " n
+    read -p "Do you want to perform a soft or hard reset? (soft/hard): " reset_type
+
+    if [[ $reset_type == "soft" ]]; then
+        git reset --soft HEAD~$n
+        echo "Soft reset to $n commits back."
+    elif [[ $reset_type == "hard" ]]; then
+        git reset --hard HEAD~$n
+        echo "Hard reset to $n commits back."
+    else
+        echo "Invalid option. Please choose 'soft' or 'hard'."
+    fi
+}
+
 main() {
     echo "Starting Setup"
     install_homebrew
 
     read -p "Do you want to install all tools at once? (y/n): " install_all
     # Use indexed arrays for items and their corresponding functions
-    items=("blueutil" "Maccy" "Add Github User" "Setup Aliases" "Setup Custom Functions" "Displayplacer" "Zsh Autosuggestions")
-    functions=("install_blueutil" "add_maccy" "add_github_user" "setup_aliases" "setup_custom_functions" "install_displayplacer" "install_zsh_autosuggestions")
+    items=("blueutil" "Maccy" "Add Github User" "Setup Aliases" "Setup Custom Functions" "Displayplacer" "Zsh Autosuggestions" "Move Back N Commits")
+    functions=("install_blueutil" "add_maccy" "add_github_user" "setup_aliases" "setup_custom_functions" "install_displayplacer" "install_zsh_autosuggestions" "move_back_n_commits")
 
     if [[ $install_all == "y" ]]; then
         # Install all tools by iterating over the functions
